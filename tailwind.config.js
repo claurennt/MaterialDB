ef281256b276980fb51056f952fcc3f194b74835
claurennt <c.benedetti1989@gmail.com>
2023-12-19 18:55:33 +0100

    add new styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,8 +6,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: { 100: '#735eff', 200: '#5241BE' },
-        secondary: '#FD5244',
+        primary: { 100: '#735eff', 200: '#1F51FF' },
+        secondary: { 100: '#fd7544', 200: '#FD5244' },
       },
       fontFamily: {
         sans: ['var(--font-jost)'],
