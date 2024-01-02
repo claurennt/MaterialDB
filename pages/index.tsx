0b89dffe47d7a91fa5392d3dd1bf9386bed033cd
claurennt <c.benedetti1989@gmail.com>
2024-01-02 12:21:53 +0100

    feat: add loading spinner, center footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,17 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import DBClient from 'utils/server/DBClient';
-import Head from 'next/head';
 import { GetServerSideProps } from 'next';
-
-import Topics from 'components/Topics';
-
-import AuthLinks from 'components/AuthLinks';
-import NewLinkForm from 'components/NewLinkForm';
-
+import Head from 'next/head';
 import { Jost } from 'next/font/google';
+import { useSession } from 'next-auth/react';
+import { Session } from 'next-auth';
+import { BounceLoader } from 'react-spinners';
 
 import { ITopic } from 'types/mongoose';
-import { Session } from 'next-auth';
-
-import { useSession } from 'next-auth/react';
+import Topics from 'components/Topics';
+import DBClient from 'utils/server/DBClient';
+import AuthLinks from 'components/AuthLinks';
+import NewLinkForm from 'components/NewLinkForm';
 import Subtitle from 'components/Subtitle';
 import Topic from 'models/Topic';
 import MainTitle from 'components/MainTitle';
@@ -25,45 +22,59 @@ type HomeProps = { currentTopics: ITopic[]; session: Session };
 const jost = Jost({ subsets: ['latin'], variable: '--font-inter' });
 
 const Home: React.FunctionComponent<HomeProps> = ({ currentTopics }) => {
+<<<<<<< Updated upstream
   const { data: session } = useSession();
 
   const [retrievedTopics, setRetrievedTopics] = useState<ITopic[]>(
     currentTopics?.length ? currentTopics : []
   );
+=======
+>>>>>>> Stashed changes
   const [open, setOpen] = useState<boolean>(false);
 
+  const { data: session } = useSession();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+
+  const retrievedTopics: ITopic[] | Session['user']['topics'] =
+    currentTopics?.length ? currentTopics : session?.user?.topics || [];
+
   useEffect(() => {
-    // after login update the topics taken from the logged in user
-    if (session) {
-      setRetrievedTopics(session.user.topics);
+    let timerId: number;
+    if (session) setIsLoading(false);
+    else {
+      timerId = window.setTimeout(() => setIsLoading(false), 500);
     }
+    return () => clearTimeout(timerId);
   }, [session]);
 
   return (
-    <div className={`${jost.variable} font-sans`}>
+    <>
       <Head>
         <title>MaterialDB</title>
         <link rel='icon' href='/logo.ico' />
       </Head>
+      <div className={'min-h-full'}>
+        <div className={`${jost.variable} font-sans pb-2 `}>
+          <main className='flex flex-col items-center gap-y-10 text-center pt-20'>
+            <MainTitle />
+            <Subtitle setOpen={setOpen} />
+            <BounceLoader color='#FD5244' loading={isLoading} size={100} />
+            {!session && !isLoading && <AuthLinks />}
 
-      <main className='flex flex-col items-center gap-y-10 text-center pt-20'>
-        <MainTitle />
-        <Subtitle setOpen={setOpen} />
+            {retrievedTopics?.length > 0 ? (
+              <Topics topicsArray={retrievedTopics} />
+            ) : null}
 
-        {!session && <AuthLinks />}
-
-        {retrievedTopics?.length > 0 ? (
-          <Topics topicsArray={retrievedTopics} />
-        ) : null}
-
-        {open && session && (
-          <NewLinkForm type='topic' open={open} setOpen={setOpen} />
-        )}
-      </main>
-      <footer className='text-center  mt-20 mb-5'>
-        made with love by claurennt
-      </footer>
-    </div>
+            {open && session && (
+              <NewLinkForm type='topic' open={open} setOpen={setOpen} />
+            )}
+          </main>
+          <footer className='text-center absolute bottom-5 w-full'>
+            made with love by claurennt
+          </footer>
+        </div>
+      </div>
+    </>
   );
 };
 
