2a36afb084722d964eb27be1e0907583423b7cf0
claurennt <c.benedetti1989@gmail.com>
2024-01-23 19:07:37 +0100

    test: update MainTitle.cy.tsx, add Subtitle component tests

--- cypress/components/MainTitle.cy.tsx
+++ cypress/components/MainTitle.cy.tsx
@@ -1,34 +1,38 @@
-/// <reference types="cypress" />
-
 import React from 'react';
 import MainTitle from '../../components/MainTitle';
 import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
 
 describe('<MainTitle />', () => {
   it('should render the user name when there is a session', () => {
-    const session = {
-      user: {
-        name: 'Claudia',
-        email: 'claudia@gmail.com',
-        image: null,
-      },
-      expires: '3000-01-01T00:00:00.000Z',
-    };
+    // Load the fixture
+    cy.fixture('session').then((session: Session) => {
+      // Mount the component with the SessionProvider
+      cy.mount(
+        <SessionProvider session={session}>
+          <MainTitle />
+        </SessionProvider>
+      );
 
+      // Assert the rendered content
+      cy.get('h1').should(
+        'have.text',
+        `Welcome back to your MaterialDB ${session.user!.name}!`
+      );
+    });
+  });
+
+  it('should only say hello without a session', () => {
+    const session = null;
+
+    // Mount the component with the SessionProvider
     cy.mount(
       <SessionProvider session={session}>
         <MainTitle />
       </SessionProvider>
     );
-    cy.contains(`Welcome back to your MaterialDB ${session.user.name}!`);
-  });
 
-  it('should only say hello without a session', () => {
-    cy.mount(
-      <SessionProvider session={null}>
-        <MainTitle />
-      </SessionProvider>
-    );
+    // Assert the rendered content
     cy.get('h1').should('have.text', 'Welcome to MaterialDB!');
   });
 });
