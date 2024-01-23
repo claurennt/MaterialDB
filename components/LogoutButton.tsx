ba247980a1aba8a0b8d835c6b0c3ef2641726e71
claurennt <c.benedetti1989@gmail.com>
2024-01-23 19:09:23 +0100

    fix: remove spaces and unnecessary code

--- components/LogoutButton.tsx
+++ components/LogoutButton.tsx
@@ -8,7 +8,7 @@ const LogoutButton = () => {
 
   return (
     <button
-      className='bg-primary-100  text-sm hover:bg-blue-700 text-white hover:bg-primary-neon focus:bg-primary-neon font-bold py-1 px-2 rounded-full m-5 absolute right-0 top-0'
+      className='bg-primary-100  text-sm text-white hover:bg-primary-neon focus:bg-primary-neon font-bold py-1 px-2 rounded-full m-5 absolute right-0 top-0'
       onClick={async (e) => {
         e.preventDefault();
 
--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,5 +1,5 @@
 const Tag = ({ tag }) => (
-  <button className='tag px-2 text-xs mx-1 border-border-color p-1 text-lg hover:bg-secondary-300 focus:bg-primary-neon text-white rounded-full'>
+  <button className='tag px-2  mx-1 border-border-color p-1 text-lg hover:bg-secondary-300 focus:bg-primary-neon text-white rounded-full'>
     {tag}
   </button>
 );
