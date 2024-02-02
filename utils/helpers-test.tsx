4790aeba61921df5d1ddc3e185f4c946e8894b33
claurennt <c.benedetti1989@gmail.com>
2024-02-02 17:18:33 +0100

    test: create next-router-mock, add SessionWrapper to RTL render

---
+++ utils/helpers-test.tsx
@@ -0,0 +1,39 @@
+import React, { ReactElement } from 'react';
+import { render, RenderOptions } from '@testing-library/react';
+import { SessionProvider } from 'next-auth/react';
+import { Session } from 'next-auth';
+
+const session: Session = {
+  user: {
+    id: '1',
+    name: 'Claudia',
+    email: 'claudia@gmail.com',
+    image: null,
+    topics: [],
+  },
+  expires: '3000-01-01T00:00:00.000Z',
+};
+
+const WithSession: React.FunctionComponent<{
+  children: React.ReactNode;
+}> = ({ children }) => {
+  return <SessionProvider session={session}>{children}</SessionProvider>;
+};
+const renderWithSession = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: WithSession, ...options });
+
+const WithoutSession: React.FunctionComponent<{
+  children: React.ReactNode;
+}> = ({ children }) => (
+  <SessionProvider session={null}>{children}</SessionProvider>
+);
+
+const renderWithoutSession = (
+  ui: ReactElement,
+  options?: Omit<RenderOptions, 'wrapper'>
+) => render(ui, { wrapper: WithoutSession, ...options });
+
+export * from '@testing-library/react';
+export { renderWithSession, renderWithoutSession };
